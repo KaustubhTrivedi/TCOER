9d73477a6b75362a02fefc77aeea07d8e9894d2e
Kaustubh Trivedi <kaus12tri@gmail.com>
2020-07-17 14:45:02 +0530

    Add Select Query

---
+++ SL-I DBMS/Lecture2/SelectQuery.sql
@@ -0,0 +1,9 @@
+SELECT NameofEmployee from employee WHERE job='Manager';
+SELECT NameofEmployee,job from employee WHERE job!='Manager';
+SELECT NameofEmployee,Salary from employee WHERE Salary>20000;
+SELECT NameofEmployee,Salary from employee WHERE Salary>=20000;
+SELECT NameofEmployee, from employee WHERE deparmentNo=10;
+SELECT NameofEmployee,Salary from employee WHERE Salary<20000;
+
+
+
