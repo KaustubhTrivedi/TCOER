15b0063e92b942bffb3151157ac5cfb88016dc9e
Kaustubh Trivedi <kaus12tri@gmail.com>
2020-07-17 13:54:39 +0530

    Edits in the created file

--- SL-I DBMS/Lecture2/CreateTableEMP.sql
+++ SL-I DBMS/Lecture2/CreateTableEMP.sql
@@ -1,1 +1,1 @@
-CREATE TABLE Employee(EmpNo int primary key, Name VARCHAR (20) NOT  NULL ,HireDate DATE, deparmentNo INT NOT NULL,Salary FLOAT (10,2) DEFAULT 5000.00)
+CREATE TABLE Employee(EmpNo int primary key, NameofEmployee VARCHAR (20) NOT  NULL ,HireDate DATE, deparmentNo INT NOT NULL,Salary FLOAT (10,2) DEFAULT 5000.00)
